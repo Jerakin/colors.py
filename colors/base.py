06db98e0626480de6554a8fb39625167de6adc1c
Jerakin <the_rensvind@hotmail.com>
2024-10-05 12:25:09 +0200

    using explicit propeties/setters; added typehints; remove 2.7 support

--- colors/base.py
+++ colors/base.py
@@ -4,7 +4,7 @@ colors.base
 Convert colors between rgb, hsv, and hex, perform arithmetic, blend modes,
 and generate random colors within boundaries.
 """
-from __future__ import print_function
+from __future__ import annotations
 import colorsys
 import random as random_
 
@@ -14,20 +14,12 @@ __all__ = ('Color', 'HSVColor', 'RGBColor', "RGBFloatColor", 'HexColor', 'ColorW
 HEX_RANGE = frozenset('0123456789abcdef')
 
 
-def color_decorator(cls):
-    attributes = cls.__dict__
-    if 'Meta' in attributes and hasattr(attributes['Meta'], 'properties'):
-        for _index, prop in enumerate(attributes['Meta'].properties):
-            # Assign pretty getters to each property name
-            setattr(cls, prop, property(lambda self, index=_index: self._color[index]))
-    return cls
-
-
-class Color(object):
+class Color:
     """ Abstract base class for all color types. """
+    _color: list
 
     @property
-    def hex(self):
+    def hex(self) -> HexColor:
         """ Hex is used the same way for all types. """
         return HexColor('{:02x}{:02x}{:02x}'.format(*[int(x) for x in self.rgb]))
 
@@ -39,7 +31,7 @@ class Color(object):
     def hsv(self):
         raise NotImplementedError
 
-    def multiply(self, other):
+    def multiply(self, other: Color) -> RGBColor:
         self_rgb = self.rgb
         other_rgb = other.rgb
         return RGBColor(
@@ -50,7 +42,7 @@ class Color(object):
 
     __mul__ = multiply
 
-    def add(self, other):
+    def add(self, other: Color) -> RGBColor:
         self_rgb = self.rgb
         other_rgb = other.rgb
         return RGBColor(
@@ -61,7 +53,7 @@ class Color(object):
 
     __add__ = add
 
-    def divide(self, other):
+    def divide(self, other: Color) -> RGBColor:
         self_rgb = self.rgb
         other_rgb = other.rgb
         if 0 in other_rgb:
@@ -72,10 +64,9 @@ class Color(object):
             self_rgb.blue / float(other_rgb.blue),
         )
 
-    __div__ = divide
     __truediv__ = divide
 
-    def subtract(self, other):
+    def subtract(self, other: Color) -> RGBColor:
         self_rgb = self.rgb
         other_rgb = other.rgb
         return RGBColor(
@@ -86,7 +77,7 @@ class Color(object):
 
     __sub__ = subtract
 
-    def screen(self, other):
+    def screen(self, other: Color) -> RGBColor:
         self_rgb = self.rgb
         other_rgb = other.rgb
         return RGBColor(
@@ -95,7 +86,7 @@ class Color(object):
             255 - (((255 - self_rgb.blue) * (255 - other_rgb.blue)) / 255.0),
         )
 
-    def difference(self, other):
+    def difference(self, other: Color) -> RGBColor:
         self_rgb = self.rgb
         other_rgb = other.rgb
         return RGBColor(
@@ -104,42 +95,34 @@ class Color(object):
             abs(self_rgb.blue - other_rgb.blue),
         )
 
-    def overlay(self, other):
+    def overlay(self, other: Color) -> RGBColor:
         return self.screen(self.multiply(other))
 
     def invert(self):
         return self.difference(RGBColor(255, 255, 255))
 
-    def __eq__(self, other):
+    def __eq__(self, other: Color) -> bool:
         self_rgb = self.rgb
         other_rgb = other.rgb
         return self_rgb.red == other_rgb.red and self_rgb.green == other_rgb.green and self_rgb.blue == other_rgb.blue
 
-    def __contains__(self, item):
+    def __contains__(self, item: Color) -> bool:
         return item in self._color
 
-    def __ne__(self, other):
+    def __ne__(self, other) -> bool:
         return not self.__eq__(other)
 
     def __iter__(self):
         """ Treat the color object as an iterable to iterate over color values"""
         return iter(self._color)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._color)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return ', '.join(map(str, self._color))
 
-    def __repr__(self):
-        base = u'<{} {}>'
-        properties = [
-            '{}: {}'.format(prop, getattr(self, prop)) for prop in self.Meta.properties
-        ]
-        return base.format(self.__class__.__name__, ', '.join(properties))
 
-
-@color_decorator
 class HSVColor(Color):
     """ Hue Saturation Value """
 
@@ -153,10 +136,37 @@ class HSVColor(Color):
         if h >= 1:
             h -= int(h)
 
-        self._color = h, s, v
+        self._color = [h, s, v]
+
+    def __repr__(self) -> str:
+        return f"HSVColor(h={self.hue}, s={self.saturation}, v={self.value})"
 
     @property
-    def rgb(self):
+    def hue(self) -> float:
+        return self._color[0]
+
+    @hue.setter
+    def hue(self, value: float):
+        self._color[0] = value
+
+    @property
+    def saturation(self) -> float:
+        return self._color[1]
+
+    @saturation.setter
+    def saturation(self, value: float):
+        self._color[1] = value
+
+    @property
+    def value(self) -> float:
+        return self._color[2]
+
+    @value.setter
+    def value(self, value: float):
+        self._color[2] = value
+
+    @property
+    def rgb(self) -> RGBColor:
         return RGBColor(*map(lambda c: c * 255, colorsys.hsv_to_rgb(*self._color)))
 
     @property
@@ -164,62 +174,105 @@ class HSVColor(Color):
         return self
 
     @property
-    def float(self):
+    def float(self) -> RGBFloatColor:
         return RGBFloatColor(*map(lambda c: c, colorsys.hsv_to_rgb(*self._color)))
 
-    class Meta:
-        properties = ('hue', 'saturation', 'value')
 
-
-@color_decorator
 class RGBColor(Color):
     """ Red Green Blue colors represented in a 0 - 255 range"""
 
-    def __init__(self, r=0, g=0, b=0):
-        self._color = round(r), round(g), round(b)
+    def __init__(self, r: int = 0, g: int = 0, b: int = 0):
+        self._color = [round(r), round(g), round(b)]
         for c in self._color:
             if 0 > c or c > 255:
                 raise ValueError('Color values must be between 0 and 255')
 
+    def __repr__(self) -> str:
+        return f"RGBColor(r={self.red}, g={self.green}, b={self.blue})"
+
     @property
-    def rgb(self):
+    def rgb(self) -> RGBColor:
         return self
 
     @property
-    def hsv(self):
+    def hsv(self) -> HSVColor:
         return HSVColor(*colorsys.rgb_to_hsv(*map(lambda c: c / 255, self._color)))
 
     @property
-    def float(self):
+    def float(self) -> RGBFloatColor:
         return RGBFloatColor(*map(lambda c: c / 255, self._color))
 
-    class Meta:
-        properties = ('red', 'green', 'blue')
+    @property
+    def red(self) -> int:
+        return self._color[0]
+
+    @red.setter
+    def red(self, value: int):
+        self._color[0] = value
+
+    @property
+    def green(self) -> int:
+        return self._color[1]
+
+    @green.setter
+    def green(self, value: int):
+        self._color[1] = value
+
+    @property
+    def blue(self) -> int:
+        return self._color[2]
+
+    @blue.setter
+    def blue(self, value: int):
+        self._color[2] = value
 
 
-@color_decorator
 class RGBFloatColor(Color):
     """ Red Green Blue colors represented in a 0-1 range """
-    def __init__(self, r=0.0, g=0.0, b=0.0):
-        self._color = r, g, b
+    def __init__(self, r: float = 0.0, g: float = 0.0, b: float = 0.0):
+        self._color =  [r, g, b]
         for c in self._color:
             if 0 > c or c > 1:
                 raise ValueError('Color values must be between 0 and 1')
 
+    def __repr__(self) -> str:
+        return f"RGBFloatColor(r={self.red}, g={self.green}, b={self.blue})"
+
     @property
-    def hsv(self):
+    def hsv(self) -> HSVColor:
         return HSVColor(*colorsys.rgb_to_hsv(*map(lambda c: c, self._color)))
 
     @property
-    def rgb(self):
+    def rgb(self) -> RGBColor:
         return self.hsv.rgb
 
     @property
-    def float(self):
+    def float(self) -> RGBFloatColor:
         return self
 
-    class Meta:
-        properties = ('red', 'green', 'blue')
+    @property
+    def red(self) -> float:
+        return self._color[0]
+
+    @red.setter
+    def red(self, value: float):
+        self._color[0] = value
+
+    @property
+    def green(self) -> float:
+        return self._color[1]
+
+    @green.setter
+    def green(self, value: float):
+        self._color[1] = value
+
+    @property
+    def blue(self) -> float:
+        return self._color[2]
+
+    @blue.setter
+    def blue(self, value: float):
+        self._color[2] = value
 
 
 class HexColor(RGBColor):
@@ -227,7 +280,6 @@ class HexColor(RGBColor):
 
     Warning: accuracy is lost when converting a color to hex
     """
-
     def __init__(self, _hex='000000'):
         if not type(_hex) == str:
             raise ValueError("Hex must be string")
@@ -242,47 +294,45 @@ class HexColor(RGBColor):
         self._color = _hex[:2], _hex[2:4], _hex[4:6]
 
     @property
-    def rgb(self):
+    def rgb(self) -> RGBColor:
         return RGBColor(*[int(c, 16) for c in self._color])
 
     @property
-    def hsv(self):
+    def hsv(self) -> HSVColor:
         return self.rgb.hsv
 
     @property
-    def hex(self):
+    def hex(self) -> HexColor:
         return self
 
     @property
-    def float(self):
+    def float(self) -> RGBFloatColor:
         return RGBFloatColor(*[int(c, 16)/255.0 for c in self._color])
 
-    def __str__(self):
+    def __str__(self) -> str:
         return '{}{}{}'.format(*self._color)
 
 
-class ColorWheel(object):
+class ColorWheel:
     """ Iterate random colors distributed relatively evenly around the color wheel."""
-    def __init__(self, start=0):
+    def __init__(self, start: float = 0):
         # A 1.1 shift is identical to 0.1
         if start >= 1:
             start -= 1
         self._phase = start
 
-    def __iter__(self):
+    def __iter__(self) -> ColorWheel:
         return self
 
-    def __next__(self):
+    def __next__(self) -> HSVColor:
         shift = (random_.random() * 0.1) + 0.1
         self._phase += shift
         if self._phase >= 1:
             self._phase -= 1
         return HSVColor(self._phase, 1, 0.8)
 
-    next = __next__  # for Python 2
 
-
-def random():  # This name might be a bad idea?
+def random() -> HSVColor:  # This name might be a bad idea?
     """ Generate a random color."""
     return HSVColor(random_.random(), random_.random(), random_.random())
 
